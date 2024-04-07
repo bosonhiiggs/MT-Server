9f1578dd64697183eb871824b06da9d814e90b6a
hiiggs <yurasik@gmail.com>
2024-04-07 16:12:50 +0300

    Add right anwer in model Answer

--- musicTrainee/catalog/admin.py
+++ musicTrainee/catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, Content, Text, Module
+from .models import Course, Content, Text, Module, File, Image, Video, Answer, Question
 
 
 @admin.register(Course)
@@ -24,3 +24,30 @@ class ContentAdmin(admin.ModelAdmin):
 class TextAdmin(admin.ModelAdmin):
     list_display = ['title', 'content',]
 
+
+@admin.register(File)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ['title', 'file',]
+
+
+@admin.register(Image)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ['title', 'file',]
+
+
+@admin.register(Video)
+class TextAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url',]
+
+
+class QuestionsInline(admin.TabularInline):
+    model = Answer
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text']
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['question', 'text']
--- musicTrainee/catalog/models.py
+++ musicTrainee/catalog/models.py
@@ -141,6 +141,7 @@ class Question(ItemBase):
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
+    is_true = models.BooleanField(default=False)
 
     def __str__(self):
         return self.text
