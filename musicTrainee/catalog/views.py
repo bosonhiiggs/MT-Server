5ef694ff1e896d6ac1278688bbd0ea24a8328f8b
YURGEN <yurasikp@gmail.com>
2024-04-19 14:33:33 +0300

    Add new templates for details modules

--- musicTrainee/catalog/views.py
+++ musicTrainee/catalog/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpRequest, HttpResponse
+﻿from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
@@ -30,10 +30,23 @@ class CourseModules(ListView):
     def get_queryset(self):
         course_slug = self.request.path.split('/')[2]
         course_choice = Course.objects.filter(slug=course_slug)[0]
-        queryset_module = Module.objects.select_related("course").filter(course=course_choice).all()
+        queryset_modules = Module.objects.select_related("course").filter(course=course_choice).all()
+
+        queryset_modules_content = dict()
+        # queryset_content = Content.objects.select_related("module").filter(module=queryset_modules).all()
+
+        # queryset_content = dict()
+        for module in queryset_modules:
+            content_module = Content.objects.filter(module=module).all()
+            queryset_modules_content[module] = content_module
+            for content_object in content_module:
+                print(content_object.item)
+
+        # print(queryset_modules_content)
+
         queryset = {
             "course": course_choice,
-            "modules": queryset_module,
+            "content": queryset_modules_content,
         }
         return queryset
 
@@ -42,5 +55,15 @@ class CourseModules(ListView):
 
 
 class CourseModuleDetails(DetailView):
+    def get_object(self, queryset=None):
+        # course_slug = self.request.path.split('/')
+        course_slug = self.request.path.split('/')[2]
+        course_content_pk = self.request.path.split('/')[4]
+        # print(course_slug)
+        # print(course_content_pk)
+        queryset = Content.objects.filter(pk=course_content_pk).all()
+        return queryset
+
+    context_object_name = "object"
     template_name = "catalog/course-modules-details.html"
-    queryset = Module.objects.select_related("course")
+
