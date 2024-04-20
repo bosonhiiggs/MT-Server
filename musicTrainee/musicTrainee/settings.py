3b4bbfbf3759aabf5284ee186cbffd6b8b14b33e
YURGEN <yurasikp@gmail.com>
2024-04-20 23:43:15 +0300

    Add API app

--- musicTrainee/musicTrainee/settings.py
+++ musicTrainee/musicTrainee/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
     'catalog.apps.CatalogConfig',
+    'musicApi.apps.MusicapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
