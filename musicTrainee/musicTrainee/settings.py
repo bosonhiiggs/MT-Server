782616793175a10544f89f7ce7978ce478d9741e
YURGEN <yurasikp@gmail.com>
2024-05-09 13:32:47 +0300

    Edit env-template; database path; add database-template

--- musicTrainee/musicTrainee/settings.py
+++ musicTrainee/musicTrainee/settings.py
@@ -97,8 +97,8 @@ WSGI_APPLICATION = 'musicTrainee.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': DATABASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db-template.sqlite3',
+        'NAME': DATABASE_DIR / 'db-template.sqlite3',
     }
 }
 
